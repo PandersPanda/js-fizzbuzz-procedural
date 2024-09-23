bc2bd8ab9714c9693c3bbbd9e31b8087ae8df858
Anders Hagen Ottersland <anderh0709@gmail.com>
2024-09-23 08:45:31 +0200

    done

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,7 +2,29 @@ const answer = []
 
 // Write your code below this line
 
-
+for (i = 1; i <= 15; i++) {
+    let dividable3 = false
+    let dividable5 = false
+    if (i % 3 === 0) {
+      dividable3 = true
+    }
+    if (i % 5 === 0) {
+      dividable5 = true
+    }
+    if (dividable3 && dividable5) {
+      answer.push('FizzBuzz')
+      continue
+    }
+    if (dividable3) {
+      answer.push('Fizz')
+      continue
+    }
+    if (dividable5) {
+      answer.push('Buzz')
+      continue
+    }
+    answer.push(i)
+  }
 
 
 
